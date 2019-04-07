dce31f760909f2d1904a16e0ce7d16179e32e8ff
Sumedha <suma@Sumedhas-MacBook-Pro.local>
2019-04-07 18:22:09 +0530

    Added react skeleton components

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ const logger = (store) => (next) => (action) => {
 	console.group(action.type)
 		console.log('Dispatched', action)
 		const newState = next(action)
-		console.log('New state is :', newState)
+		console.log('New state is :', store.getState())
 	console.groupEnd()
 	return newState
 }
