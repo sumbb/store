6c2b7778812d8f47bf85126e00e257bcb3a09a2b
Sumedha Birajdar <birajdar@sumedhas-mbp.corp.adobe.com>
2019-04-17 17:54:55 +0530

    Added Async call for Intial data fetch

--- index.js
+++ index.js
@@ -135,6 +135,17 @@ function receiveDataAction(todos, goals) {
 	}
 }
 
+function handleInitialData() {
+	return (dispatch) => {
+		Promise.all([
+			API.fetchTodos(),
+			API.fetchGoals()
+		]).then(([todos, goals]) => {
+			dispatch(receiveDataAction(todos, goals))
+		})
+	}
+}
+
 function todos (state = [], action) {
 	switch (action.type ) {
 		case ADD_TODO :
